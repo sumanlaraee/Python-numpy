4c6bb64163bb200989266a661fcefbaba9f286ba
suman bai <Sumanbai.bscssef20@iba-suk.edu.pk>
2023-08-31 04:09:10 -0700

    i have added two more files

---
+++ numpy_tut_5.py
@@ -0,0 +1,9 @@
+import numpy as np 
+'''x=np.array([[1,2,3], [3,4, 5],[6, 7, 8]], np.int32)
+np.savetxt("test.txt", x)'''
+
+y=np.loadtxt("test.txt")
+print(y)
+
+
+#on command promt to run use :python filename.py and uh must have python installed on system 
